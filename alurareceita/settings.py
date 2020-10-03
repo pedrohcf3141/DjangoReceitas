a6ca37158d9c67b991504a52b3bff3a4044caa8a
Pedro Henrique Cerqueira Fernandes <pedrohcf3.14@gmail.com>
2020-10-03 18:09:35 -0300

    incluso o app pessoas
    curso 02 aula 02

--- alurareceita/settings.py
+++ alurareceita/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'receitas',
+    'pessoas',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +77,11 @@ WSGI_APPLICATION = 'alurareceita.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'alura_receita_2',
+        'USER' : 'postgres',
+        'PASSWORD' : 'postgres',
+        'HOST': 'localhost'
     }
 }
 
